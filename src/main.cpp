fd62b1a37e049c398638551402b1524bdadeac7f
Baohai Xu <baohai.xu@hotmail.com>
2024-07-04 17:34:13 +0200

    thresholds as variales

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,75 @@
 #define PIN_OUT 2
 #define PIN_SCK 3
+#define LED_GRUEN 4
+#define LED_GELB 5
+#define LED_ROT 6
+#define BUZZER 7
+#define SENSOR_PIN A0
+
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <RCSwitch.h>
+
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
-#include <RCSwitch.h>
-
 RCSwitch rcSwitch = RCSwitch();
-
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-// Drucksensor
+// Struktur für Schwellenwerte
+struct Thresholds
+{
+  int low;
+  long high;
+  const char *message;
+  int ledPin;
+  bool buzzer;
+};
 
-int Sensorpin = A0; // Definition, an welcher Stelle am Mikrocontroller der Sensorwert ausgelesen werden soll
+// Größter möglicher Wert für einen int
+const long INT_MAX = 2147483647L;
 
-// int a;
+// Initialisiere Schwellenwerte
+Thresholds thresholds[] = {
+    {1631, INT_MAX, "K N I C K", LED_ROT, true},
+    {1351, 1500, " VERDREHT", LED_ROT, true},
+    {1131, 1350, " NORMAL", LED_GRUEN, false},
+    {0, 1150, " entfernt", LED_GELB, true}};
 
 void setup()
 {
-
   Serial.begin(9600);
-  // Sendemodul an dem digitalen PIN 10 angeschlossen.
+
   rcSwitch.enableTransmit(10);
 
-  pinMode(7, OUTPUT); // setze digitalpin 7 auf output
-
-  pinMode(4, OUTPUT); // LED Grün
-  pinMode(5, OUTPUT); // LED GELB
-  pinMode(7, OUTPUT); // LED ROT
+  // Initialisiere LED-Pins und Buzzer
+  int ledPins[] = {LED_GRUEN, LED_GELB, LED_ROT, BUZZER};
+  for (int i = 0; i < 4; i++)
+  {
+    pinMode(ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], LOW);
+  }
 
   pinMode(PIN_OUT, INPUT);
   pinMode(PIN_SCK, OUTPUT);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // eventuell 0x3D - benutze den I2C_Scanner
 
-  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // eventuell 0x3D - benutze den I2C_Scanner
 }
 
 void loop()
 {
-  digitalWrite(7, LOW); // buzzer++
+  digitalWrite(BUZZER, LOW);
+  digitalWrite(LED_GRUEN, LOW);
+  digitalWrite(LED_GELB, LOW);
+  digitalWrite(LED_ROT, LOW);
 
-  digitalWrite(4, LOW); // LED orange
-  digitalWrite(5, LOW); // lED ROT
-  digitalWrite(6, LOW); // LED grün
-
-  // wait for the current reading to finish
+  // warte auf den aktuellen Messwert
   while (digitalRead(PIN_OUT))
   {
   }
 
-  // read 24 bits
+  // Lese 24 Bits
   long result = 0;
   for (int i = 0; i < 24; i++)
   {
@@ -64,10 +82,10 @@ void loop()
     }
   }
 
-  // get the 2s compliment
+  // Konvertiere zu 2er-Komplement
   result = result ^ 0x800000;
 
-  // pulse the clock line 3 times to start the next pressure reading
+  // Pulse die Clock Line 3 mal, um die nächste Druckmessung zu starten
   for (byte i = 0; i < 3; i++)
   {
     digitalWrite(PIN_SCK, HIGH);
@@ -75,12 +93,7 @@ void loop()
   }
 
   Serial.println(result);
-  int resultat;
-  resultat = result / 10000;
-
-  ////   mittleren Wert ermitteln///////////////////////////
-
-  // resultat = map(resultat, 780, 1800, 780, 1800);
+  int resultat = result / 10000;
 
   display.clearDisplay();
   display.setTextColor(WHITE);
@@ -99,67 +112,23 @@ void loop()
   char sendValue[10];
   itoa(resultat, sendValue, 10); // Konvertiert 'resultat' in eine Zeichenkette
 
-  if (resultat > 1630)
+  // Überprüfe Schwellenwerte
+  for (size_t i = 0; i < sizeof(thresholds) / sizeof(thresholds[0]); i++)
   {
-
-    Serial.println(resultat);
-    display.println("K N I C K");
-    digitalWrite(7, HIGH);
-    digitalWrite(5, HIGH);
-
-    // Senden von resultat
-    rcSwitch.send(sendValue);
-  }
-
-  if ((resultat > 1350) and resultat < 1500)
-  {
-
-    Serial.println(resultat);
-    display.println(" VERDREHT");
-    digitalWrite(7, HIGH);
-    digitalWrite(5, HIGH);
-
-    // Senden von resultat
-    rcSwitch.send(sendValue);
-  }
-
-  if ((resultat > 1130) and resultat < 1350)
-  {
-
-    Serial.println(resultat);
-
-    display.println(" NORMAL");
-    digitalWrite(4, HIGH);
-
-    // Senden von resultat
-    rcSwitch.send(sendValue);
-  }
-
-  if (resultat < 1150)
-  {
-    Serial.println(resultat);
-    Serial.println("entfernt");
-    display.println(" entfernt");
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-
-    // Senden von resultat
-    rcSwitch.send(sendValue);
-
-    // if (resultat=758);
-    // Serial.print(resultat);
-    // if (resultat=0);
-    // Serial.print(resultat);
-
-    // delay(1000);
+    if (resultat >= thresholds[i].low && resultat <= thresholds[i].high)
+    {
+      Serial.println(resultat);
+      display.println(thresholds[i].message);
+      digitalWrite(thresholds[i].ledPin, HIGH);
+      if (thresholds[i].buzzer)
+      {
+        digitalWrite(BUZZER, HIGH);
+      }
+      rcSwitch.send(sendValue);
+      break;
+    }
   }
 
   display.display();
   delay(500);
 }
-
-// Arduino Anschluss
-// OLED GND – Arduino GND
-//  OLED VCC – Arduino 5V
-// OLED SCL – Arduino A5
-//  OLED SDA – Arduino A4
